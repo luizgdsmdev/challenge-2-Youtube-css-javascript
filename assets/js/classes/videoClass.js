5fa3a38c7380febe2048be54516c5ae85a08cd31
Luiz Messias <luizgdsmdev@gmail.com>
2025-07-28 09:30:49 -0300

    json update

--- assets/js/classes/videoClass.js
+++ assets/js/classes/videoClass.js
@@ -3,13 +3,15 @@ export class Video{
     thumb;
     channel;
     channelIcon;
+    videoLink;
     videoID;
 
-    constructor(name, thumb, channel, channelIcon, videoID){
+    constructor(name, thumb, channel, channelIcon, videoLink, videoID){
         this.name = name;
         this.thumb = thumb;
         this.channel = channel;
         this.channelIcon = channelIcon;
+        this.videoLink = videoLink;
         this.videoID = videoID;
     }
 }
--- assets/js/videoHandler.js
+++ assets/js/videoHandler.js
@@ -4,9 +4,46 @@ import { Video } from "./classes/videoClass.js"
 
 const videosListFromJson = await fetchVideosData(); 
 
+
+let videos__sectionRecomendedVideo = document.getElementById("videos__section-recomended-video");
 function loadRecomendedVideos(videosList){
+    let htmlLoadRecomendedVideos;
+    htmlLoadRecomendedVideos = videosList.map(video => {
+        let newVideo = new Video(video.name, video.thumb, video.channel, video.channelIcon, video.videoLink, video.videoID)
+        return `
+            <li>
+                <div id="latest-news-card" class="latest-news-card">
+                    <img loading="lazy" id="latest-news-card-thumb" class="latest-news-card-thumb" alt="" src="${video.thumb}" >
+                    <div id="latest-news-card-details" class="latest-news-card-details">
+                        <div id="latest-news-card-details-text" class="latest-news-card-details-text">
+                            
+                            <div id="latest-news-card-details-text-tile" class="latest-news-card-details-text-tile">
+                                <h3>${video.name}</h3>
+                                <img loading="lazy" id="latest-news-card-thumb-dots-menu" class="latest-news-card-thumb-dots-menu" title="Options" alt="Options" src="./assets/svg/svg-three-dots.svg" >
+                            </div>
+
+
+                            <div id="latest-news-card-details-text-2" class="latest-news-card-details-text-2">
+                                <img loading="lazy" id="latest-news-card-details-channel-img" class="latest-news-card-details-channel-img" alt="" src="${video.channelIcon}" >
+                                <div id="latest-news-card-details-block" class="latest-news-card-details-block">
+                                    <div id="latest-news-card-details-block-in" class="latest-news-card-details-block-in">
+                                        <p>${video.channel}</p>
+                                        <img loading="lazy" id="latest-news-card-details-verify-channel" class="latest-news-card-details-verify-channel" src="" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        `;
+    });
+
+    videos__sectionRecomendedVideo.innerHTML += htmlLoadRecomendedVideos.join("");
 
 }
+
+loadRecomendedVideos(videosListFromJson.videos_top_line.videos_recomend)
 
 
 
